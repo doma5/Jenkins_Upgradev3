72e91cf8b9999be2f000ed48826efc1fcdc419b4
doma5 <domantas543@gmail.com>
2022-05-02 00:10:43 +0300

    Added additional Test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "SUPER TEST";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
